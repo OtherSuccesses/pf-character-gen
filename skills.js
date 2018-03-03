c7cef530bf6f28429bbd385a2caacb90827f8ec4
OtherSuccesses <viskaslietuvai@gmail.com>
2018-03-03 12:50:16 -0500

    added skills list, unlinked, actually added.

---
+++ skills.js
@@ -0,0 +1,179 @@
+let skillsList[{
+	id: 1,
+	name: "Acrobatics",
+	ability: "dexterity",
+	untrained: true
+},{
+	id: 2,
+	name: "Appraise",
+	ability:"intelligence",
+	untrained: true
+},{
+	id: 3,
+	name: "Bluff",
+	ability: "charisma",
+	untrained: true
+},{
+	id: 4,
+	name: "Climb",
+	ability: "strength",
+	untrained: true
+},{
+	id: 5,
+	name: "Craft",
+	ability:"intelligence",
+	subtypes: ["Alchemy", "Armor", "Art", "Bows", "Building", "Calligraphy", "Clothing", "Jewelry", "Vehicles", "Traps", "Weapons"], 
+	untrained: true
+},{
+	id: 6,
+	name: "Diplomacy",
+	ability: "charisma",
+	untrained: true
+},{
+	id: 7,
+	name: "Disable Device",
+	ability: "dexterity",
+	untrained: false
+},{
+	id: 8,
+	name: "Disguise",
+	ability: "charisma",
+	untrained: true
+},{
+	id: 9,
+	name: "Escape Artist",
+	ability: "dexterity",
+	untrained: true
+},{
+	id: 10,
+	name: "Fly",
+	ability: "dexterity",
+	untrained: true
+},{
+	id: 11,
+	name: "Handle Animal",
+	ability: "charisma",
+	untrained: true
+},{
+	id: 12,
+	name: "Heal",
+	ability: "wisdom",
+	untrained: true
+},{
+	id: 13,
+	name: "Intimidate",
+	ability: "charisma",
+	untrained: true
+},{
+	id: 14,
+	name: "Knowledge (Arcana)",
+	ability:"intelligence",
+	untrained: false
+},{
+	id: 15,
+	name: "Knowledge (Dungeoneering)",
+	ability:"intelligence",
+	untrained: false
+},{
+	id: 16,
+	name: "Knowledge (Engineering)",
+	ability:"intelligence",
+	untrained: false
+},{
+	id: 17,
+	name: "Knowledge (Geography)",
+	ability:"intelligence",
+	untrained: false
+},{
+	id: 18,
+	name: "Knowledge (History)",
+	ability:"intelligence",
+	untrained: false
+},{
+	id: 19,
+	name: "Knowledge (Local)",
+	ability:"intelligence",
+	untrained: false
+},{
+	id: 20,
+	name: "Knowledge (Nature)",
+	ability:"intelligence",
+	untrained: false
+},{
+	id: 21,
+	name: "Knowledge (Nobility)",
+	ability:"intelligence",
+	untrained: false
+},{
+	id: 22,
+	name: "Knowledge (Planes)",
+	ability:"intelligence",
+	untrained: false
+},{
+	id: 23,
+	name: "Knowledge (Religion)",
+	ability:"intelligence",
+	untrained: false
+},{
+	id: 24,
+	name: "Linguistics",
+	ability:"intelligence",
+	untrained: false
+},{
+	id: 25,
+	name: "Perception",
+	ability: "wisdom",
+	untrained: true
+},{
+	id: 26,
+	name: "Perform",
+	ability: "charisma",
+	subtypes: ["Act", "Comedy", "Dance", "Keyboard", "Oratory", "Percussion", "String", "Wind", "Sing"],
+	untrained: true
+},{
+	id: 27,
+	name: "Profession",
+	ability: "wisdom",
+	subtypes: ["Architect", "Lawyer", "Brewer", "Clerk", "Cook", "Courtesan", "Doctor", "Farmer", "Fisherman", "Gambler", "Herbalist", "Hunter", "Innkeeper", "Librarian", "Merchant", "Miner", "Sailor", "Scribe", "Stable Master", "Soldier"],
+	untrained: false
+},{
+	id: 28,
+	name: "Ride",
+	ability: "dexterity"
+	untrained: false
+},{
+	id: 29,
+	name: "Sense Motive",
+	ability: "wisdom",
+	untrained: true
+},{
+	id: 30,
+	name: "Sleight of Hand",
+	ability: "dexterity"
+	untrained: false
+},{
+	id: 31,
+	name: "Spellcraft",
+	ability: "intelligence"
+	untrained: false
+},{
+	id: 32,
+	name: "Stealth",
+	ability: "dexterity"
+	untrained: true
+},{
+	id: 33,
+	name: "Survival",
+	ability: "wisdom",
+	untrained: true
+},{
+	id: 34,
+	name: "Swim",
+	ability: "strength",
+	untrained: true
+},{
+	id: 35,
+	name: "Use Magic Device",
+	ability: "charisma",
+	untrained: false
+}]
