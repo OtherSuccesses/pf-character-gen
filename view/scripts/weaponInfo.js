3c4381ca3ced73633239ffeb59053da56a32e5d0
OtherSuccesses <viskaslietuvai@gmail.com>
2018-03-04 16:32:20 -0500

    all simple weapons added

--- view/scripts/weaponInfo.js
+++ view/scripts/weaponInfo.js
@@ -365,11 +365,61 @@ let weaponList = {
 				range: 0,
 				weight: 6
 			},
-			heavyMace:{
-				name: "Heavy Mace",
+			shortspear:{
+				name: "Shortspear",
 				cost: {
 					denomination: "gp",
-					amount: 12
+					amount: 1
+				},
+				dmg:{
+					small:{
+						amount: 4,
+						multiple: 1
+					},
+					medium:{
+						amount: 6,
+						multiple: 1
+					},
+					type:["P"]
+				},
+				critical:{
+					range: 1,
+					multiple: 2
+				},
+				range: 20,
+				weight: 3
+			}
+		},
+		melee2H:{
+			bayonet:{
+				name: "Bayonet",
+				cost: {
+					denomination: "gp",
+					amount: 5
+				},
+				dmg:{
+					small:{
+						amount: 4,
+						multiple: 1
+					},
+					medium:{
+						amount: 6,
+						multiple: 1
+					},
+					type:["P"]
+				},
+				critical:{
+					range: 1,
+					multiple: 2
+				},
+				range: 0,
+				weight: 1
+			},
+			longspear:{
+				name: "Longspear",
+				cost: {
+					denomination: "gp",
+					amount: 5
 				},
 				dmg:{
 					small:{
@@ -380,6 +430,30 @@ let weaponList = {
 						amount: 8,
 						multiple: 1
 					},
+					type:["P"]
+				},
+				critical:{
+					range: 1,
+					multiple: 3
+				},
+				range: 0,
+				weight: 9
+			},
+			quarterstaff:{
+				name: "Quarterstaff",
+				cost: {
+					denomination: "gp",
+					amount: 0
+				},
+				dmg:{
+					small:{
+						amount: 4,
+						multiple: 1
+					},
+					medium:{
+						amount: 6,
+						multiple: 1
+					},
 					type:["B"]
 				},
 				critical:{
@@ -387,8 +461,288 @@ let weaponList = {
 					multiple: 2
 				},
 				range: 0,
-				weight: 8
+				weight: 4
 			},
+			spear:{
+				name: "Spear",
+				cost: {
+					denomination: "gp",
+					amount: 2
+				},
+				dmg:{
+					small:{
+						amount: 6,
+						multiple: 1
+					},
+					medium:{
+						amount: 8,
+						multiple: 1
+					},
+					type:["P"]
+				},
+				critical:{
+					range: 1,
+					multiple: 3
+				},
+				range: 20,
+				weight: 6
+			},
+			boarSpear:{
+				name: "Boar Spear",
+				cost: {
+					denomination: "gp",
+					amount: 5
+				},
+				dmg:{
+					small:{
+						amount: 6,
+						multiple: 1
+					},
+					medium:{
+						amount: 8,
+						multiple: 1
+					},
+					type:["P"]
+				},
+				critical:{
+					range: 1,
+					multiple: 2
+				},
+				range: 0,
+				weight: 8
+			}
+		},
+		ranged:{
+			blowgun:{
+				name: "Blowgun",
+				cost: {
+					denomination: "gp",
+					amount: 2
+				},
+				dmg:{
+					small:{
+						amount: 1,
+						multiple: 1
+					},
+					medium:{
+						amount: 2,
+						multiple: 1
+					},
+					type:["P"]
+				},
+				critical:{
+					range: 1,
+					multiple: 2
+				},
+				range: 20,
+				weight: 1,
+				ammunition: "Dart"
+			},
+			heavyCrossbow:{
+				name: "Heavy Crossbow",
+				cost: {
+					denomination: "gp",
+					amount: 50
+				},
+				dmg:{
+					small:{
+						amount: 8,
+						multiple: 1
+					},
+					medium:{
+						amount: 10,
+						multiple: 1
+					},
+					type:["P"]
+				},
+				critical:{
+					range: 2,
+					multiple: 2
+				},
+				range: 120,
+				weight: 8,
+				ammunition: "Bolt"
+			},
+			lightCrossbow:{
+				name: "Light Crossbow",
+				cost: {
+					denomination: "gp",
+					amount: 35
+				},
+				dmg:{
+					small:{
+						amount: 6,
+						multiple: 1
+					},
+					medium:{
+						amount: 8,
+						multiple: 1
+					},
+					type:["P"]
+				},
+				critical:{
+					range: 2,
+					multiple: 2
+				},
+				range: 80,
+				weight: 4,
+				ammunition: "Bolt"
+			},
+			dart:{
+				name: "Dart",
+				cost: {
+					denomination: "sp",
+					amount: 5
+				},
+				dmg:{
+					small:{
+						amount: 3,
+						multiple: 1
+					},
+					medium:{
+						amount: 4,
+						multiple: 1
+					},
+					type:["P"]
+				},
+				critical:{
+					range: 1,
+					multiple: 2
+				},
+				range: 20,
+				weight: 0.5,
+				ammunition: "N/A"
+			},
+			javelin:{
+				name: "Javelin",
+				cost: {
+					denomination: "gp",
+					amount: 1
+				},
+				dmg:{
+					small:{
+						amount: 4,
+						multiple: 1
+					},
+					medium:{
+						amount: 6,
+						multiple: 1
+					},
+					type:["P"]
+				},
+				critical:{
+					range: 1,
+					multiple: 2
+				},
+				range: 30,
+				weight: 2,
+				ammunition: "N/A"
+			},
+			sling:{
+				name: "Sling",
+				cost: {
+					denomination: "gp",
+					amount: 0
+				},
+				dmg:{
+					small:{
+						amount: 3,
+						multiple: 1
+					},
+					medium:{
+						amount: 4,
+						multiple: 1
+					},
+					type:["B"]
+				},
+				critical:{
+					range: 1,
+					multiple: 2
+				},
+				range: 50,
+				weight: 0,
+				ammunition: "Bullets"
+			}
+		},
+		ammunition:{
+			bolt: {
+				bolt:{
+					name: "Crossbow Bolt",
+					cost: {
+						denomination: "gp",
+						amount: 1
+					},
+					weight: 1,
+					quantity: 10
+				},
+				acidBolt:{
+					name: "Acid Bolt",
+					cost: {
+						denomination: "gp",
+						amount: 40
+					},
+					weight: 0.1,
+					quantity: 1
+				},
+				drowPoisonBolt:{
+					name: "Drow Poison Bolt",
+					cost: {
+						denomination: "gp",
+						amount: 100
+					},
+					weight: 0.1,
+					quantity: 1
+				},
+				fireBolt:{
+					name: "Fire Bolt",
+					cost: {
+						denomination: "gp",
+						amount: 50
+					},
+					weight: 0.1,
+					quantity: 1
+				}
+			},
+			bullet:{
+				groaningBullets:{
+					name: "Groaning Bullets",
+					cost: {
+						denomination: "gp",
+						amount: 2
+					},
+					weight: 5,
+					quantity: 10
+				},
+				bullet:{
+					name: "Bullets",
+					cost: {
+						denomination: "sp",
+						amount: 1
+					},
+					weight: 5,
+					quantity: 10
+				},
+				smokeBullets:{
+					name: "Smoke Bullets",
+					cost: {
+						denomination: "gp",
+						amount: 100
+					},
+					weight: 5,
+					quantity: 10
+				}
+			},
+			dart:{
+				dart:{
+					name: "Darts",
+					cost: {
+						denomination: "sp",
+						amount: 5
+					},
+					weight: 1,
+					quantity: 10
+				}
+			}
 		}
 	}
 }
